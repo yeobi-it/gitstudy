c9949a15544fb59175b60e65bebc5946fc1fab7c
yeobi-it <mrd1994it@gmail.com>
2024-06-17 12:40:02 +0900

    initcommit

---
+++ src/main/java/org/example/gitstudy/GitstudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.gitstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
