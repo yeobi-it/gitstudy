6113e998869521ff92718182238a8a960358adc6
yeobi-it <mrd1994it@gmail.com>
2024-06-17 13:18:30 +0900

    add new min

--- src/main/java/org/example/gitstudy/GitstudyApplication.java
+++ src/main/java/org/example/gitstudy/GitstudyApplication.java
@@ -9,6 +9,7 @@ public class GitstudyApplication {
     public static void main(String[] args) {
         System.out.println("new line");
         System.out.println("hiy");
+        System.out.println("min");
         SpringApplication.run(GitstudyApplication.class, args);
     }
 
