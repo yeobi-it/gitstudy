bfc44d5f4408f78a85083c52825e310f48be7048
yeobi-it <mrd1994it@gmail.com>
2024-06-17 19:07:39 +0900

    tignore

--- src/main/java/org/example/gitstudy/Member.java
+++ src/main/java/org/example/gitstudy/Member.java
@@ -3,5 +3,7 @@ package org.example.gitstudy;
 public class Member {
     public String itw;
     public int ad;
+    static String tyus;
+    public int yy;
 
 }
---
+++ src/main/java/org/example/gitstudy/homework.java
@@ -0,0 +1,4 @@
+package org.example.gitstudy;
+
+public class homework {
+}
