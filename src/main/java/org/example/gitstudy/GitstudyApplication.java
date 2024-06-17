3e44013c3b393bcb0f2288a501d942062565a188
yeobi-it <mrd1994it@gmail.com>
2024-06-17 12:51:33 +0900

    add new line

--- src/main/java/org/example/gitstudy/GitstudyApplication.java
+++ src/main/java/org/example/gitstudy/GitstudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitstudyApplication {
 
     public static void main(String[] args) {
+        System.out.println("new line");
         SpringApplication.run(GitstudyApplication.class, args);
     }
 
